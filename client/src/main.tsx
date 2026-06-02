58a0662195990cc0ad194822bae62c75074cb85c
bel1c10ud <mail@bellclou.de>
2026-06-02 16:15:44 +0900

    chore(client): react-router 설정

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
+import { HashRouter, Route, Routes } from 'react-router';
+import CartPage from './pages/CartPage.tsx';
 import './styles/index.css';
+import OrderPage from './pages/OrderPage.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <HashRouter>
+      <Routes>
+        <Route path={'/'} element={<CartPage />} />
+        <Route path={'/order'} element={<OrderPage />} />
+      </Routes>
+    </HashRouter>
   </StrictMode>,
 );
