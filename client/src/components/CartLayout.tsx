0a59edf732e6c465d30069e637b0135aee8b353f
bel1c10ud <mail@bellclou.de>
2026-06-04 00:30:32 +0900

    feat(client): 장바구니 상품 선택 기능 구현

--- client/src/components/CartLayout.tsx
+++ client/src/components/CartLayout.tsx
@@ -7,6 +7,18 @@ export default function CartLayout({ data }: { data: CartItem[] }) {
     Object.fromEntries(data.map((item) => [item.cartItemId, true])),
   );
 
+  const setCartItem = (key: string, checked: boolean) => {
+    const newStorage = { ...storage };
+    newStorage[key] = checked;
+    setStorage(newStorage);
+  };
+
+  const setAllCartItem = (checked: boolean) => {
+    const newStorage = { ...storage };
+    Object.keys(newStorage).forEach((key) => (newStorage[key] = checked));
+    setStorage(newStorage);
+  };
+
   return (
     <div>
       <div>
@@ -14,14 +26,22 @@ export default function CartLayout({ data }: { data: CartItem[] }) {
         <h2>현재 {data.length}종류의 상품이 담겨있습니다.</h2>
       </div>
       <div>
-        <input type="checkbox" checked={!Object.entries(storage).some((el) => !el[1])} />
+        <input
+          type="checkbox"
+          checked={!Object.entries(storage).some((el) => !el[1])}
+          onChange={(e) => setAllCartItem(e.target.checked)}
+        />
         <div>전체선택</div>
       </div>
       <ul>
         {data.map((item) => (
           <li key={item.cartItemId}>
             <div>
-              <input type="checkbox" checked={storage[item.cartItemId]} />
+              <input
+                type="checkbox"
+                checked={storage[item.cartItemId]}
+                onChange={(e) => setCartItem(item.cartItemId, e.target.checked)}
+              />
               <button>삭제</button>
             </div>
             <div>
