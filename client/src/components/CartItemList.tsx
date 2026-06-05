afa5eb3c7ba4b7fdb098afb00f56bcadb786d775
bel1c10ud <mail@bellclou.de>
2026-06-05 16:43:00 +0900

    refactor(client): 장바구니 상품 리스트, 금액 컴포넌트 분리

---
+++ client/src/components/CartItemList.tsx
@@ -0,0 +1,43 @@
+import type { CartItem as TCartItem } from './../types';
+import CartItem from './CartItem';
+import CheckBox from './common/CheckBox';
+import Flex from './common/Flex';
+import List from './common/List';
+import Typo from './common/Typo';
+
+export default function CartItemList(props: {
+  data: TCartItem[];
+  selectedById: Record<string, boolean>;
+  onSelect: (cartItemId: string, checked: boolean) => void;
+  onSelectAll: (checked: boolean) => void;
+  refetchData: () => void;
+}) {
+  return (
+    <List
+      divider={{ header: true, item: true }}
+      header={
+        <Flex alignItems="center" gap={8} py={16}>
+          <CheckBox
+            id="check-all"
+            checked={!Object.entries(props.selectedById).some((el) => !el[1])}
+            onChange={props.onSelectAll}
+          />
+          <Typo as="label" size="s" htmlFor="check-all">
+            전체선택
+          </Typo>
+        </Flex>
+      }
+    >
+      {props.data.map((item) => (
+        <CartItem
+          key={item.cartItemId}
+          data={item}
+          checked={props.selectedById[item.cartItemId]}
+          onSelect={(checked) => props.onSelect(item.cartItemId, checked)}
+          onUpdate={props.refetchData}
+          onDelete={props.refetchData}
+        />
+      ))}
+    </List>
+  );
+}
--- client/src/components/templates/CartTemplate.tsx
+++ client/src/components/templates/CartTemplate.tsx
@@ -1,17 +1,13 @@
+import { useMemo } from 'react';
 import { useNavigate } from 'react-router';
 import type { CartItem as TCartItem } from '../../types';
-import CartItem from '../CartItem';
 import useCartItemSelection from '../../hooks/useCartItemSelection';
-import { useMemo } from 'react';
-import useCalculateCartAmount from '../../hooks/useCalculateCartAmount';
-import { formatWon } from '../../utils';
 import Typo from '../common/Typo';
 import Flex from '../common/Flex';
 import View from '../common/View';
 import Button from '../common/Button';
-import CheckBox from '../common/CheckBox';
-import List from '../common/List';
-import Image from '../common/Image';
+import CartItemList from '../CartItemList';
+import CartAmountSummary from '../CartAmountSummary';
 
 export default function CartTemplate(props: { data: TCartItem[]; refetchData: () => void }) {
   const navigate = useNavigate();
@@ -21,8 +17,6 @@ export default function CartTemplate(props: { data: TCartItem[]; refetchData: ()
   const selectedCartItems = useMemo(() => {
     return props.data.filter((cartItem) => selectedById[cartItem.cartItemId]);
   }, [props.data, selectedById]);
-
-  const { orderAmount, shippingAmount, totalAmount } = useCalculateCartAmount(selectedCartItems);
 
   return (
     <View gap={24}>
@@ -34,74 +28,20 @@ export default function CartTemplate(props: { data: TCartItem[]; refetchData: ()
           현재 {props.data.length}종류의 상품이 담겨있습니다.
         </Typo>
       </Flex>
-      <List
-        divider={{ header: true, item: true }}
-        header={
-          <Flex alignItems="center" gap={8} py={16}>
-            <CheckBox
-              id="check-all"
-              checked={!Object.entries(selectedById).some((el) => !el[1])}
-              onChange={setAllSelected}
-            />
-            <Typo as="label" size="s" htmlFor="check-all">
-              전체선택
-            </Typo>
-          </Flex>
-        }
-      >
-        {props.data.map((item) => (
-          <CartItem
-            key={item.cartItemId}
-            data={item}
-            checked={selectedById[item.cartItemId]}
-            onSelect={(checked) => setSelected(item.cartItemId, checked)}
-            onUpdate={props.refetchData}
-            onDelete={props.refetchData}
-          />
-        ))}
-      </List>
-      <List
-        divider={{ header: true, footer: true }}
-        header={
-          <Flex gap={4} py={10}>
-            <Image src={`${import.meta.env.BASE_URL}infomation.svg`} alt="infomation icon" />
-            <Typo size="s">총 주문 금액이 {formatWon(100000)} 이상일 경우 무료 배송됩니다</Typo>
-          </Flex>
-        }
-        footer={
-          <Flex justifyContent="space-between" py={10}>
-            <Typo weight="bold">총 결제 금액</Typo>
-            <Typo weight="bold" size="l" aria-label="총 결제 금액" data-value={totalAmount}>
-              {formatWon(totalAmount)}
-            </Typo>
-          </Flex>
-        }
-      >
-        <List.Item
-          left={<Typo weight="bold">주문 금액</Typo>}
-          content={
-            <Typo weight="bold" size="l" aria-label="주문 금액" data-value={orderAmount}>
-              {formatWon(orderAmount)}
-            </Typo>
-          }
-          py={10}
-        />
-        <List.Item
-          left={<Typo weight="bold">배송비</Typo>}
-          content={
-            <Typo weight="bold" size="l" aria-label="배송비" data-value={shippingAmount}>
-              {formatWon(shippingAmount)}
-            </Typo>
-          }
-          py={10}
-        />
-      </List>
+      <CartItemList
+        data={props.data}
+        selectedById={selectedById}
+        onSelect={setSelected}
+        onSelectAll={setAllSelected}
+        refetchData={props.refetchData}
+      />
+      <CartAmountSummary selectedCartItems={selectedCartItems} />
       <View.CTA>
         <Button
           variant="cta"
           onClick={() =>
             navigate('/order', {
-              state: { products: props.data.filter((cur) => selectedById[cur.cartItemId]) },
+              state: { products: selectedCartItems },
             })
           }
           disabled={!Object.entries(selectedById).some((el) => el[1])}
