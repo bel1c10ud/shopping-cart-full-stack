a22c01e31c575708b995377c85550be152607cad
bel1c10ud <mail@bellclou.de>
2026-06-05 15:46:21 +0900

    chore(client): 배포 환경 설정

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
+const GITHUB_PAGES_BASE_URL = '/shopping-cart-full-stack/'
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: GITHUB_PAGES_BASE_URL,
   plugins: [react()],
   test: {
     globals: true,
