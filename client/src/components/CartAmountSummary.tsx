3847627e76397611391e7f78b3436b00045b8b39
bel1c10ud <mail@bellclou.de>
2026-06-22 11:18:26 +0900

    refactor(client): 금액 요약 컴포넌트 공통화

--- client/src/components/CartAmountSummary.tsx
+++ client/src/components/CartAmountSummary.tsx
@@ -1,10 +1,5 @@
-import { css } from '@emotion/css';
 import useCartAmountQuery from '../hooks/queries/useCartAmountQuery';
-import { formatWon } from '../utils';
-import Flex from './common/Flex';
-import Image from './common/Image';
-import Spinner from './common/Spinner';
-import Typo from './common/Typo';
+import AmountSummaryView from './AmountSummaryView';
 
 const emptyAmount = {
   orderAmount: 0,
@@ -17,58 +12,5 @@ export default function CartAmountSummary() {
   const cartAmountQuery = useCartAmountQuery();
   const amount = cartAmountQuery.data ?? emptyAmount;
 
-  return (
-    <Flex.Column>
-      <Flex gap={4} py={10} className={headerStyle}>
-        <Image src={`${import.meta.env.BASE_URL}infomation.svg`} alt="infomation icon" />
-        <Typo size="s">총 주문 금액이 {formatWon(100000)} 이상일 경우 무료 배송됩니다</Typo>
-      </Flex>
-      <Flex.Column as="ul" className={listStyle}>
-        <Flex as="li" justifyContent="space-between" alignItems="center" py={10}>
-          <Typo weight="bold">주문 금액</Typo>
-          <Typo weight="bold" size="l" aria-label="주문 금액" data-value={amount.orderAmount}>
-            {cartAmountQuery.isFetching ? (
-              <Spinner size="s" aria-label="주문 금액 갱신 중" />
-            ) : (
-              formatWon(amount.orderAmount)
-            )}
-          </Typo>
-        </Flex>
-        <Flex as="li" justifyContent="space-between" alignItems="center" py={10}>
-          <Typo weight="bold">배송비</Typo>
-          <Typo weight="bold" size="l" aria-label="배송비" data-value={amount.shippingAmount}>
-            {cartAmountQuery.isFetching ? <Spinner size="s" aria-label="배송비 갱신 중" /> : formatWon(amount.shippingAmount)}
-          </Typo>
-        </Flex>
-      </Flex.Column>
-      <Flex justifyContent="space-between" py={10} className={footerStyle}>
-        <Typo weight="bold">총 결제 금액</Typo>
-        <Typo weight="bold" size="l" aria-label="총 결제 금액" data-value={amount.totalAmount}>
-          {cartAmountQuery.isFetching ? (
-            <Spinner size="s" aria-label="총 결제 금액 갱신 중" />
-          ) : (
-            formatWon(amount.totalAmount)
-          )}
-        </Typo>
-      </Flex>
-    </Flex.Column>
-  );
+  return <AmountSummaryView amount={amount} isLoading={cartAmountQuery.isFetching} valueSize="l" />;
 }
-
-const headerStyle = css`
-  border-bottom: 1px solid var(--color-gray-200);
-`;
-
-const footerStyle = css`
-  border-top: 1px solid var(--color-gray-200);
-`;
-
-const listStyle = css`
-  margin: 0;
-  padding: 0;
-  list-style: none;
-
-  > li {
-    list-style: none;
-  }
-`;
--- client/src/components/OrderAmountSummary.tsx
+++ client/src/components/OrderAmountSummary.tsx
@@ -1,10 +1,5 @@
-import { css } from '@emotion/css';
 import type { AmountSummary } from '../types';
-import { formatWon } from '../utils';
-import Flex from './common/Flex';
-import Image from './common/Image';
-import Spinner from './common/Spinner';
-import Typo from './common/Typo';
+import AmountSummaryView from './AmountSummaryView';
 
 interface OrderAmountSummaryProps {
   amount: AmountSummary;
@@ -12,55 +7,5 @@ interface OrderAmountSummaryProps {
 }
 
 export default function OrderAmountSummary({ amount, isLoading }: OrderAmountSummaryProps) {
-  return (
-    <Flex.Column>
-      <Flex gap={4} py={10}>
-        <Image src={`${import.meta.env.BASE_URL}infomation.svg`} alt="infomation icon" />
-        <Typo size="s">총 주문 금액이 {formatWon(100000)} 이상일 경우 무료 배송됩니다</Typo>
-      </Flex>
-      <Flex.Column as="ul" className={amountSummaryListStyle}>
-        <Flex as="li" alignItems="center" justifyContent="space-between" py={10}>
-          <Typo size="m" weight="bold">
-            주문 금액
-          </Typo>
-          <Typo size="xl" weight="bold">
-            {isLoading ? <Spinner size="s" aria-label="주문 금액 갱신 중" /> : formatWon(amount.orderAmount)}
-          </Typo>
-        </Flex>
-        <Flex as="li" alignItems="center" justifyContent="space-between" py={10}>
-          <Typo size="m" weight="bold">
-            쿠폰 할인 금액
-          </Typo>
-          <Typo size="xl" weight="bold">
-            {isLoading ? (
-              <Spinner size="s" aria-label="쿠폰 할인 금액 갱신 중" />
-            ) : (
-              `-${formatWon(amount.discountAmount)}`
-            )}
-          </Typo>
-        </Flex>
-        <Flex as="li" alignItems="center" justifyContent="space-between" py={10}>
-          <Typo size="m" weight="bold">
-            배송비
-          </Typo>
-          <Typo size="xl" weight="bold">
-            {isLoading ? <Spinner size="s" aria-label="배송비 갱신 중" /> : formatWon(amount.shippingAmount)}
-          </Typo>
-        </Flex>
-      </Flex.Column>
-      <Flex alignItems="center" justifyContent="space-between" py={10}>
-        <Typo size="m" weight="bold">
-          총 결제 금액
-        </Typo>
-        <Typo size="xl" weight="bold">
-          {isLoading ? <Spinner size="s" aria-label="총 결제 금액 갱신 중" /> : formatWon(amount.totalAmount)}
-        </Typo>
-      </Flex>
-    </Flex.Column>
-  );
+  return <AmountSummaryView amount={amount} isLoading={isLoading} showDiscount />;
 }
-
-const amountSummaryListStyle = css`
-  border-top: 1px solid var(--color-gray-200);
-  border-bottom: 1px solid var(--color-gray-200);
-`;
